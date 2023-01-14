c9b8ff1b169195406ba4c53cfb1ae95c8b3e1b7e
Aditya <anitp2018@gmail.com>
2023-01-13 22:25:18 -0700

    add service command

--- lab1/client.py
+++ lab1/client.py
@@ -1,10 +1,14 @@
-from pwn import process
+#from pwn import process
+from pwn import remote
 
 def main():
-	r = process("./server.py")
+	#r = process("./server.py")
+	r = remote("localhost", 6000)
 	r.sendline("hello")
 	line = r.recvline()
 	print(line)
+
+	r.close()
 
 if __name__ == "__main__":
 	main()
