e0ec274adb73be545fac0cc34610267c9665a58e
Aditya <anitp2018@gmail.com>
2023-01-23 22:59:17 -0700

    common commands

--- lab1/client.py
+++ lab1/client.py
@@ -1,9 +1,9 @@
-#from pwn import process
-from pwn import remote
+from pwn import process
+#from pwn import remote
 
 def main():
-	#r = process("./server.py")
-	r = remote("localhost", 6000)
+	r = process("./server.py")
+	#r = remote("localhost", 6000)
 	r.sendline("hello")
 	line = r.recvline()
 	print(line)
